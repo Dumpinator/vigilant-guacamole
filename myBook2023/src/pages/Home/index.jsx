e44fb8760580b1c91deed52bedab739d79cd466e
Jonathan <jdeboisvilliers@gmail.com>
2023-08-26 14:32:00 +0200

    fix responsive header

--- myBook2023/src/pages/Home/index.jsx
+++ myBook2023/src/pages/Home/index.jsx
@@ -7,16 +7,33 @@ export function Home() {
   return (
     <div className='home__container'>
       <div className='section-1'>
-          <header className='header__content'>
-            <div className='title'>
-              <span>PORT</span>
-              <span>FOLIO</span>
-              <p>Hello, je m’appelle Delphine et je suis basée à Paris! Passionnée par le design d’interface, 
-                j’exerce ce métier depuis 5ans. Je vous laisse découvrir mon univers et mon parcours.
-              </p>
-            </div>
-          </header>
-          <div className='header__content background'></div>
+          <div className='container'>
+            <header className='header__content'>
+              <div className='title'>
+                <span>PORT</span>
+                <span>FOLIO</span>
+                <p>Hello, je m’appelle Delphine et je suis basée à Paris! Passionnée par le design d’interface, 
+                  j’exerce ce métier depuis 5ans. Je vous laisse découvrir mon univers et mon parcours.
+                </p>
+              </div>
+              <div className='test'>
+                <div className='toto'>
+                  <div className='to'></div>
+                </div>
+                <div className='tutu'>
+                  <div className='tu'></div>
+                </div>
+              </div>
+            </header>
+          </div>
+      </div>
+      <div className='section-2'>
+        <div className='left'>
+          <div className='card'></div>
+          <div className='card'></div>
+          <div className='card'></div>
+        </div>
+        <div className='right'></div>
       </div>
     </div>
   )
